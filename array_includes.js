cb6e7239eb4d488fb994c12467df29a51bddcb39
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-04-22 13:59:39 -0400

    Find term in string

---
+++ array_includes.js
@@ -0,0 +1,12 @@
+//l
+
+let list = ['Todd', 'Darie', 'Jonathan', 'Zach', 'Manny'];
+
+function findString(arr) {
+    if (arr.indexOf('Ahmed') !== -1) {
+        return "Yes";
+    } else {
+    return "No"
+    }
+
+console.log(findString(list));
